498086c9b53434d0fe8b80f202024be5a9620c4c
zhonghuajian <zhonghuajian@kingsoft.com>
2016-07-19 22:59:22 +0800

    [Freeload] 断点续传测试

--- app/src/main/java/com/shineson/jason/freeload/MainActivity.java
+++ app/src/main/java/com/shineson/jason/freeload/MainActivity.java
@@ -1,23 +1,32 @@
 package com.shineson.jason.freeload;
 
+import android.app.Activity;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.Button;
 
 import com.freeload.jason.core.DownloadThreadType;
+import com.freeload.jason.core.IReceipt;
 import com.freeload.jason.core.RequestQueue;
 import com.freeload.jason.core.Response;
 import com.freeload.jason.toolbox.DownloadRequestManager;
 import com.freeload.jason.toolbox.EscapeReceipt;
 import com.freeload.jason.toolbox.Freeload;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
 
     private RequestQueue requestQueue = null;
     private DownloadRequestManager request = null;
+
+    private Button mStart = null;
+    private Button mEnd = null;
+    private Button mResume = null;
+
+    private String receipt = "";
 
     private String downloadUrl = "http://sw.bos.baidu.com/sw-search-sp/software/f726db3f1f943/QQ_8.4.18380.0_setup.exe";
 
@@ -25,26 +34,50 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
 
         requestQueue = Freeload.newRequestQueue(this);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
+        mStart = (Button) findViewById(R.id.start);
+        mStart.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-//                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-//                        .setAction("Action", null).show();
-
                 request = DownloadRequestManager.create()
                         .setDownloadId(1)
                         .setDownloadUrl(downloadUrl)
                         .setDownloadThreadType(DownloadThreadType.DOUBLETHREAD)
-                        .setListener(new Response.Listener<EscapeReceipt>() {
+                        .setListener(new Response.Listener<IReceipt>() {
                             @Override
-                            public void onProgressChange(EscapeReceipt s) {
-                                System.out.println(s.toString());
+                            public void onProgressChange(IReceipt s) {
+                                receipt = s.getReceipt();
+                                System.out.println(s.getReceipt());
+                            }
+                        })
+                        .addRequestQueue(requestQueue);
+            }
+        });
+
+        mEnd = (Button) findViewById(R.id.stop);
+        mEnd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                request.cancel();
+            }
+        });
+
+        mResume = (Button) findViewById(R.id.resume);
+        mResume.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                request = DownloadRequestManager.create()
+                        .setDownloadId(1)
+                        .setDownloadUrl(downloadUrl)
+                        .setEscapeReceipt(receipt)
+                        .setDownloadThreadType(DownloadThreadType.DOUBLETHREAD)
+                        .setListener(new Response.Listener<IReceipt>() {
+                            @Override
+                            public void onProgressChange(IReceipt s) {
+                                receipt = s.getReceipt();
+                                System.out.println(s.getReceipt());
                             }
                         })
                         .addRequestQueue(requestQueue);
